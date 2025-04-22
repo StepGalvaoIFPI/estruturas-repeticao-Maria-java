e7c31c4f89ab2a6e65fa9d8f30280f3bf91c962e
Maria-java <catzs.2023112lcom0043@aluno.ifpi.edu.br>
2025-04-22 19:37:29 -0300

    Update Q1i.java

--- Q1i.java
+++ Q1i.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Q1{
-  public static void main(String[] args){
-    System.out.print("Ola Mundo");
-  }
+	public static void main(String[] args) {
+	    Scanner entrada = new Scanner(System.in);
+	    System.out.print("Digite a velocidade:");
+	    int velocidade = Integer.valueOf(entrada.nextLine());
+	    
+	    if(velocidade > 80){
+	        System.out.println("Vermelho");
+	    }else{
+	        System.out.println("Verde");
+	    }
+	}
 }
